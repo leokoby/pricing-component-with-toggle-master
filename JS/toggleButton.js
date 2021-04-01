adb6f23c074efecdeb1d2d2606fa78dc3797fb58
leokoby <leokoby1@gmail.com>
2021-04-01 19:36:13 -0300

    1#commit

--- JS/toggleButton.js
+++ JS/toggleButton.js
@@ -1,9 +1,7 @@
 const btnSwitch = document.querySelector('#switch input')
-
 const priceBasic = document.querySelector('#price-basic h1')
 const priceProfessional = document.querySelector('#price-profissional h1')
 const priceMaster = document.querySelector('#price-master h1')
-
 const valoresM = [19.99, 24.99, 39.99]
 const valoresA = [199.99, 249.99, 399.99]
 
@@ -11,8 +9,7 @@ priceBasic.innerHTML = `$${valoresA[0]}`;
 priceProfessional.innerHTML = `$${valoresA[1]}`;
 priceMaster.innerHTML = `$${valoresA[2]}`;
 
-
-btnSwitch.addEventListener('click', function(){
+function controle(){
     if(priceBasic.innerHTML === `$${valoresA[0]}` && priceProfessional.innerHTML === `$${valoresA[1]}` && priceMaster.innerHTML === `$${valoresA[2]}`)
     {
         
@@ -24,4 +21,15 @@ btnSwitch.addEventListener('click', function(){
         priceProfessional.innerHTML = `$${valoresA[1]}`;
         priceMaster.innerHTML = `$${valoresA[2]}`;
     }
-})
+}
+
+
+
+
+btnSwitch.addEventListener('keydown', function(e){
+    if(e.key == 'Enter'){
+        controle()
+    }
+});
+
+btnSwitch.addEventListener('click', controle);
